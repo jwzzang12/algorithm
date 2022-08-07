3098170ef1298fdc62d9d9f677ca555f5496ed62
jwzzang12 <78199999+jwzzang12@users.noreply.github.com>
2022-08-07 23:12:52 +0900

    Time: 117 ms (15.08%), Space: 43 MB (65.05%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkTree = function(root) {
+    let result = root.left.val + root.right.val
+    return result = result === root.val ? true : false
+};

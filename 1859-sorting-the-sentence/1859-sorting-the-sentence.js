232d23997786bbbac5d7df74c33f92eba71759be
Jiwon Moon <78199999+jwzzang12@users.noreply.github.com>
2022-08-15 20:29:28 +0900

    Time: 72 ms (75.53%), Space: 42.3 MB (22.76%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -6,6 +6,6 @@ var sortSentence = function (s) {
   return s
     .split(" ")
     .sort((a, b) => a.slice(-1) - b.slice(-1))
-    .map((word) => word.slice(0, -1))
+    .map(word => word.slice(0, -1))
     .join(" ");
 };

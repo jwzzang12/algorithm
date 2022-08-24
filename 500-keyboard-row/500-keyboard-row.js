2fa02eaeae7119576dc111eb0025d919ff5f855d
Jiwon Moon <78199999+jwzzang12@users.noreply.github.com>
2022-08-24 14:26:00 +0900

    Time: 78 ms (69.94%), Space: 41.8 MB (68.09%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = function (words) {
+  const firstRow = "qwertyuiop".split("");
+  const secondRow = "asdfghjkl".split("");
+  const thirdRow = "zxcvbnm".split("");
+  const result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let wordsArr = words[i].toLowerCase().split("");
+    // wordsArr=[ 'h', 'e', 'l', 'l', 'o' ]
+    if (wordsArr.every((r) => firstRow.includes(r))) {
+      result.push(words[i]);
+    } else if (wordsArr.every((r) => secondRow.includes(r))) {
+      result.push(words[i]);
+    } else if (wordsArr.every((r) => thirdRow.includes(r))) {
+      result.push(words[i]);
+    }
+  }
+  return result;
+};

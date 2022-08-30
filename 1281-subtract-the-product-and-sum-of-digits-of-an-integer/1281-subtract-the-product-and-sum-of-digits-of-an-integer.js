cfe88a2f07ca236022a5edd6bc6efe717f442f49
Jiwon Moon <78199999+jwzzang12@users.noreply.github.com>
2022-08-30 13:50:57 +0900

    Time: 66 ms (89.28%), Space: 42.1 MB (67.91%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -2,10 +2,10 @@
  * @param {number} n
  * @return {number}
  */
-var subtractProductAndSum = function (n) {
-  const arr = n.toString().split("").map(Number);
-  const plus = arr.reduce((a, b) => a + b);
-  const multiple = arr.reduce((a, b) => a * b);
-
+const subtractProductAndSum = function (n) {
+  const arr = n.toString().split("");
+  const newArr = arr.map((e) => parseInt(e));
+  const multiple = newArr.reduce((a, b) => a * b);
+  const plus = newArr.reduce((a, b) => a + b);
   return multiple - plus;
 };
